87bfdb902b8506bae44fd826163a8c6558cf096b
illus <illusory.racer01@mail.ru>
2024-10-17 03:40:38 +1000

    add components

---
+++ store.tsx
@@ -0,0 +1,64 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+const defaultState = {
+    product: []
+}
+
+const action = {type: '', payload: ''}
+
+const reducer = (state = defaultState, action) => {
+    console.log(state)
+    switch (action.type) {
+        case "ADD_PRODUCT":
+            return ADD_PRODUCT(state, action)
+        case "DELETE_PRODUCT":
+            return DELETE_PRODUCT(state, action)
+        case "DELETE_ALL_PRODUCT":
+            return DELETE_ALL_PRODUCT(state, action)
+        default:
+            return state
+    }
+}
+
+export const store = configureStore({
+    reducer
+})
+
+const ADD_PRODUCT = (state, action) => {
+    if (state.product.length !== 0) {
+        let checkCard = false
+        for (let i = 0; i < state.product.length; i++) {
+            if ( state.product[i].card.id === action.payload.card.id) {
+                const newPosts = state.product.map((card) => (
+                    card.card.id === action.payload.card.id
+                        ? { ...card, sumItems: card.sumItems + 1 }
+                        : card
+                ));
+                checkCard = true
+                return {...state, product: newPosts}
+            }
+        }
+        if (!checkCard) {
+            return {...state, product: [...state.product, { sumItems: 1, card: action.payload.card}]}
+        }
+    } else {
+        return {...state, product: [...state.product, { sumItems: 1, card: action.payload.card}]}
+    }
+}
+
+const DELETE_PRODUCT = (state, action) => {
+    if(action.payload.card.sumItems === 1) {
+        DELETE_ALL_PRODUCT(state, action)
+    } else {
+        const newPosts = state.product.map((card) => (
+            card.card.id === action.payload.card.id
+                ? { ...card, sumItems: card.sumItems - 1 }
+                : card
+        ));
+        return {...state, product: newPosts}
+    }
+}
+
+const DELETE_ALL_PRODUCT = (state, action) => {
+    return {...state, product: state.product.filter(card => (card.card.id !== action.payload))}
+}
